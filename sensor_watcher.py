381c07c31d3f0621db3a98f5e12558115e63d440
Chrystiano Araújo <chrystianosaraujo@gmail.com>
2016-02-03 01:44:20 -0200

    First commit of the class used to receive and handle anemometer sensor interruptions

---
+++ sensor_watcher.py
@@ -0,0 +1,8 @@
+class AnemometerWatcher:
+    def __init__(self, input_pin, data):
+        self.input_pin = input_pin
+        self.data = data
+
+    def start_watching(self):
+        while True:
+            pass
