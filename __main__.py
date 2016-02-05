7907ccdf5560b44dd1407b26984fdd9904c66694
Chrystiano Araújo <chrystianosaraujo@gmail.com>
2016-02-04 22:29:07 -0200

    Adding an application config file and its parser.

--- __main__.py
+++ __main__.py
@@ -1,33 +1,39 @@
+from app_config import ApplicationConfigData, InvalidConfigFileError
 from sensor_data import AnemometerData
 from sensor_watcher import AnemometerWatcher
 from data_sync import WindServerSynchronizer
+
 import threading
+import os
 
-# Raspberry input pin
-# TODO(CARAUJO): read this info from a configuration file
-INPUT_ANEMOMETER_PIN = 27
-
-# 3 seconds. This value must just be used in test environment
-# TODO(CARAUJO): read this info from a configuration file
-SYNC_INTERVAL = 3
+# Defines config filename
+package_dir = os.path.dirname(os.path.abspath(__file__))
+config_fn = os.path.join(package_dir, "config.json")
 
 def main():
     print("... RealtimeWind has been started")
 
-    speed_data   = AnemometerData()
-    speed_sensor = AnemometerWatcher(INPUT_ANEMOMETER_PIN, speed_data)
-
-    # The threading.Event is used to signal non-daemon threads that the 
-    # main thread has died.
-    stop_event  = threading.Event()
-    server_sync = WindServerSynchronizer(speed_data, SYNC_INTERVAL, stop_event)
-
     try:
+        config_data  = ApplicationConfigData(config_fn)
+        speed_data   = AnemometerData()
+        speed_sensor = AnemometerWatcher(config_data.raspberry_anemometer_pin(),
+                                         speed_data)
+
+        # The threading.Event is used to signal non-daemon threads that the 
+        # main thread has died.
+        stop_event  = threading.Event()
+        server_sync = WindServerSynchronizer(speed_data,
+                                             config_data.server_syncronization_interval(),
+                                             stop_event)
+
         server_sync.run()
 
         # The main thread will keep watching sensors for its whole
         # life cycle.
         speed_sensor.start_watching()
+
+    except InvalidConfigFileError, error:
+      print(error)
 
     except(KeyboardInterrupt, SystemExit):
         cleanup(stop_event)
---
+++ app_config.py
@@ -0,0 +1,54 @@
+""" Application config data. """
+
+import json
+
+class ApplicationConfigData:
+    def __init__(self, config_fn):
+        """ Loads the application config data from a json file.
+
+        config_fn is the filename of a configuration json file.
+        """
+
+        self._parse_config_file(config_fn)
+        self._check_config_data()
+
+    def raspberry_anemometer_pin(self):
+        """ Returns the GPIO pin that must be used as the anemometer
+        input pin.
+        """
+        return self.anemometer_pin
+
+    def server_syncronization_interval(self):
+        """ Returns the interval time, in milliseconds, used to send wind
+        conditions to the server.
+        """
+        return self.sync_interval
+
+    def _parse_config_file(self, config_fn):
+        try:
+            json_file = open(config_fn, "r")
+            json_data = json.load(json_file)
+
+            self.anemometer_pin = int(json_data["raspberry"]["anemometer_pin"])
+            self.sync_interval  = float(json_data["server"]["sync_interval"])
+
+        except IOError:
+            error_msg = "The configuration file does not exist - "
+            raise InvalidConfigFileError(error_msg + config_fn)
+
+        except (ValueError, KeyError):
+            raise InvalidConfigFileError("The configuration file is not valid.")
+
+    def _check_config_data(self):
+        if not self.anemometer_pin or not self.sync_interval:
+            raise InvalidConfigFileError("The configuration file is not valid.")
+
+
+class InvalidConfigFileError(Exception):
+    """ Class used to represent errors while loading the config file. """
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.__class__.__name__ + ": " + self.msg
