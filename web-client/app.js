b7a0ac95fd466ae6600ab0a22e9eca482a57e04e
Renato D. Prado <renatoprado21@gmail.com>
2016-02-07 17:48:57 -0200

    Show current date

--- web-client/app.js
+++ web-client/app.js
@@ -1,3 +1,8 @@
 (function(){
     var app = angular.module('RaspWind', [])
+
+    app.controller('RaspWindController', function(){
+        this.windSpeed = 10;
+        this.date = new Date();
+    })
 })();
