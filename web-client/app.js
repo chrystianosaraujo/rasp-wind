a1e2a348635c6a910824678577d704015e39206c
Renato D. Prado <renatoprado21@gmail.com>
2016-02-19 15:12:23 -0200

    Use Angular Chart
    Angular chart is a set of directives for chart.js.
    
    Currently using a chart with example data

--- web-client/app.js
+++ web-client/app.js
@@ -1,8 +1,38 @@
 (function(){
-    var app = angular.module('RaspWind', [])
+    var app = angular.module('RaspWind', ['chart.js'])
 
     app.controller('RaspWindController', function(){
         this.windSpeed = 10;
         this.date = new Date();
-    })
+    });
+
+    app.controller('LineCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+        $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+        $scope.series = ['Series A', 'Series B'];
+        $scope.data = [
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
+        ];
+
+        $scope.onClick = function (points, evt) {
+            console.log(points, evt);
+        };
+
+        $scope.onHover = function (points) {
+            if (points.length > 0) {
+                console.log('Point', points[0].value);
+            } else {
+                console.log('No point');
+            }
+        };
+
+        $timeout(function () {
+            $scope.labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+            $scope.data = [
+                [28, 48, 40, 19, 86, 27, 90],
+                [65, 59, 80, 81, 56, 55, 40]
+            ];
+            $scope.series = ['Series C', 'Series D'];
+        }, 3000);
+  }]);
 })();
